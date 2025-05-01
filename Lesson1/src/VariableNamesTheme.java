e9e02bc9d4b87e240b8604551fc18e4ac5d356ad
Эду Эду1 <0563166@gmail.com>
2025-05-01 16:54:29 +0300

    RaznyePeremennye

---
+++ Lesson1/src/VariableNamesTheme.java
@@ -0,0 +1,102 @@
+public class VariableNamesTheme
+{
+    public static void main(String[] args) {
+        // РАЗНЫЕ ПЕРЕМЕННЫЕ
+        int cifra = 7;
+        int summaChisel = 15;
+        int proizvedenieCifr = 56;
+        int maksimalnoeChislo = 999;
+        int kolichestvoDesyatkov = 8;
+        double vesSobaki = 12.5;
+        int iskhodnoeChislo = 123;
+        double procentPoVkladu = 7.5;
+        char simvolAmpersand = '&';
+        int kodOshibki = 404;
+        String tipSoobshcheniya = "Информация";
+        int chisloNuley = 3;
+        int unikalnoeChislo = 2025;
+        int sluchaynoeChislo = (int)(Math.random() * 100);
+        String matematicheskoeVyrazhenie = "2 * (3 + 4)";
+        int schetVIgre = 5;
+        int maksimalnayaDlina = 120;
+        String punktMenu = "Настройки";
+        double stoimostKofe = 180.0;
+        String nachalnayaData = "2025-05-01";
+        String okonchanieDiapazona = "2025-06-01";
+        String imyaRabotnikaMesyatsa = "Екатерина Смирнова";
+        String nazvanieElektronnoyKnigi = "Искусственный интеллект: Введение";
+        String razmer = "M";
+        int vmestimost = 100;
+        int schetchik = 0;
+        String putDoFaila = "C:/Users/User/Documents/file.txt";
+        int kolichestvoChiselVStroki = 4;
+
+        // BOOLEAN-ПЕРЕМЕННЫЕ
+        boolean sotniRavny = false;
+        boolean kompyuterVkluchen = true;
+        boolean estRavnyeCifry = true;
+        boolean sluzhbaSozdana = true;
+        boolean failPustoy = false;
+        boolean soedinenieAktivnoe = true;
+        boolean novyi = true;
+        boolean elektronnayaPochtaDeystvitelnaya = true;
+        boolean imeyutsyaUnikalnyeStroki = false;
+
+        // АББРЕВИАТУРЫ
+        String oldUUID = "123e4567-e89b-12d3-a456-426614174000";
+        String proizvooditelOperativnoyPamyati = "Kingston";
+        String emkostZhestkogoDiska = "1TB";
+        String opredelenieHTTP = "Hypertext Transfer Protocol";
+        String shortURL = "bit.ly/3eXmpl";
+        String newClientID = "CLNT-2025-001";
+        int asciiLength = 128;
+
+        // Вывод в консоль
+        System.out.println("Цифра: " + cifra);
+        System.out.println("Сумма чисел: " + summaChisel);
+        System.out.println("Произведение цифр: " + proizvedenieCifr);
+        System.out.println("Максимальное число: " + maksimalnoeChislo);
+        System.out.println("Количество десятков: " + kolichestvoDesyatkov);
+        System.out.println("Вес собаки: " + vesSobaki);
+        System.out.println("Исходное число: " + iskhodnoeChislo);
+        System.out.println("Процент по вкладу: " + procentPoVkladu);
+        System.out.println("Символ &: " + simvolAmpersand);
+        System.out.println("Код ошибки: " + kodOshibki);
+        System.out.println("Тип сообщения: " + tipSoobshcheniya);
+        System.out.println("Число нулей: " + chisloNuley);
+        System.out.println("Уникальное число: " + unikalnoeChislo);
+        System.out.println("Случайное число: " + sluchaynoeChislo);
+        System.out.println("Математическое выражение: " + matematicheskoeVyrazhenie);
+        System.out.println("Счёт в игре: " + schetVIgre);
+        System.out.println("Максимальная длина: " + maksimalnayaDlina);
+        System.out.println("Пункт меню: " + punktMenu);
+        System.out.println("Стоимость кофе: " + stoimostKofe);
+        System.out.println("Начальная дата: " + nachalnayaData);
+        System.out.println("Окончание диапазона: " + okonchanieDiapazona);
+        System.out.println("Имя работника месяца: " + imyaRabotnikaMesyatsa);
+        System.out.println("Название электронной книги: " + nazvanieElektronnoyKnigi);
+        System.out.println("Размер: " + razmer);
+        System.out.println("Вместимость: " + vmestimost);
+        System.out.println("Счётчик: " + schetchik);
+        System.out.println("Путь до файла: " + putDoFaila);
+        System.out.println("Количество чисел в строке: " + kolichestvoChiselVStroki);
+
+        System.out.println("Сотни равны? " + sotniRavny);
+        System.out.println("Компьютер включен? " + kompyuterVkluchen);
+        System.out.println("Есть равные цифры? " + estRavnyeCifry);
+        System.out.println("Служба создана? " + sluzhbaSozdana);
+        System.out.println("Файл пустой? " + failPustoy);
+        System.out.println("Соединение активное? " + soedinenieAktivnoe);
+        System.out.println("Новый? " + novyi);
+        System.out.println("Электронная почта действительная? " + elektronnayaPochtaDeystvitelnaya);
+        System.out.println("Имеются уникальные строки? " + imeyutsyaUnikalnyeStroki);
+
+        System.out.println("Старый UUID: " + oldUUID);
+        System.out.println("Производитель ОЗУ: " + proizvooditelOperativnoyPamyati);
+        System.out.println("Емкость HDD: " + emkostZhestkogoDiska);
+        System.out.println("HTTP: " + opredelenieHTTP);
+        System.out.println("Короткий URL: " + shortURL);
+        System.out.println("Новый ID клиента: " + newClientID);
+        System.out.println("Количество символов в ASCII: " + asciiLength);
+    }
+}
