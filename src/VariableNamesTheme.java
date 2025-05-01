18c70ebe104338c19f7feaf2e8ef863736c7f778
Эду Эду1 <0563166@gmail.com>
2025-05-01 17:25:55 +0300

    RaznyePeremennye

---
+++ src/VariableNamesTheme.java
@@ -0,0 +1,180 @@
+public class VariableNamesTheme
+{
+    public static void main(String[] args) {
+        // Цифра
+        int digit = 7;
+        System.out.println("Цифра = " + digit);
+
+        // Сумма чисел
+        int digitSum = 5 + 5;
+        System.out.println("Сумма чисел = " + digitSum);
+
+        // Произведение цифр
+        int digitProduct = 7 * 8;
+        System.out.println("Произведение цифр = " + digitProduct);
+
+        // Максимальное число
+        int maxNumber = 999;
+        System.out.println("Максимальное число = " + maxNumber);
+
+        // Количество десятков
+        int tensCount = 45 / 10;
+        System.out.println("Количество десятков = " + tensCount);
+
+        // Вес собаки
+        double dogWeight = 12.4;
+        System.out.println("Вес собаки = " + dogWeight);
+
+        // Исходное число
+        int originalNumber = 123;
+        System.out.println("Исходное число = " + originalNumber);
+
+        // Процент по вкладу
+        double depositRate = 6.5;
+        System.out.println("Процент по вкладу = " + depositRate);
+
+        // Символ &
+        char ampersand = '&';
+        System.out.println("Символ & = " + ampersand);
+
+        // Код ошибки
+        int errorCode = 404;
+        System.out.println("Код ошибки = " + errorCode);
+
+        // Тип сообщения
+        String messageType = "Warning";
+        System.out.println("Тип сообщения = " + messageType);
+
+        // Число нулей
+        int zeroCount = 2;
+        System.out.println("Число нулей = " + zeroCount);
+
+        // Уникальное число
+        int uniqueNumber = 2025;
+        System.out.println("Уникальное число = " + uniqueNumber);
+
+        // Случайное число
+        int randomNumber = (int)(Math.random() * 100);
+        System.out.println("Случайное число = " + randomNumber);
+
+        // Математическое выражение
+        String mathExpression = "3 + 4 * 2";
+        System.out.println("Математическое выражение = " + mathExpression);
+
+        // Счет в игре
+        int gameScore = 12;
+        System.out.println("Счет в игре = " + gameScore);
+
+        // Максимальная длина
+        int maxLength = 255;
+        System.out.println("Максимальная длина = " + maxLength);
+
+        // Пункт меню
+        String menuItem = "Файл";
+        System.out.println("Пункт меню = " + menuItem);
+
+        // Стоимость кофе
+        double coffeePrice = 199.99;
+        System.out.println("Стоимость кофе = " + coffeePrice);
+
+        // Начальная дата
+        String startDate = "2025-05-01";
+        System.out.println("Начальная дата = " + startDate);
+
+        // Окончание диапазона
+        String endRange = "2025-06-30";
+        System.out.println("Окончание диапазона = " + endRange);
+
+        // Имя работника месяца
+        String employeeOfMonth = "Анна Иванова";
+        System.out.println("Имя работника месяца = " + employeeOfMonth);
+
+        // Название электронной книги
+        String ebookTitle = "Java для начинающих";
+        System.out.println("Название электронной книги = " + ebookTitle);
+
+        // Размер
+        String size = "L";
+        System.out.println("Размер = " + size);
+
+        // Вместимость
+        int capacity = 64;
+        System.out.println("Вместимость = " + capacity);
+
+        // Счетчик
+        int counter = 0;
+        System.out.println("Счетчик = " + counter);
+
+        // Путь до файла
+        String filePath = "C:/data/info.txt";
+        System.out.println("Путь до файла = " + filePath);
+
+        // Количество чисел в строке
+        int numberCountInLine = 3;
+        System.out.println("Количество чисел в строке = " + numberCountInLine);
+
+        // Сотни равны?
+        boolean areHundredsEqual = false;
+        System.out.println("Сотни равны? " + areHundredsEqual);
+
+        // Компьютер включен?
+        boolean isComputerOn = true;
+        System.out.println("Компьютер включен? " + isComputerOn);
+
+        // Есть равные цифры?
+        boolean hasEqualDigits = true;
+        System.out.println("Есть равные цифры? " + hasEqualDigits);
+
+        // Служба создана?
+        boolean isServiceCreated = true;
+        System.out.println("Служба создана? " + isServiceCreated);
+
+        // Файл пустой?
+        boolean isFileEmpty = false;
+        System.out.println("Файл пустой? " + isFileEmpty);
+
+        // Соединение активное?
+        boolean isConnectionActive = true;
+        System.out.println("Соединение активное? " + isConnectionActive);
+
+        // Новый?
+        boolean isNew = true;
+        System.out.println("Новый? " + isNew);
+
+        // Электронная почта действительная?
+        boolean isEmailValid = true;
+        System.out.println("Электронная почта действительная? " + isEmailValid);
+
+        // Имеются уникальные строки?
+        boolean hasUniqueStrings = false;
+        System.out.println("Имеются уникальные строки? " + hasUniqueStrings);
+
+        // Старый UUID
+        String oldUUID = "a1b2c3d4-e5f6-7890-1234-abcdef123456";
+        System.out.println("Старый UUID = " + oldUUID);
+
+        // Производитель оперативной памяти
+        String ramManufacturer = "Corsair";
+        System.out.println("Производитель ОЗУ = " + ramManufacturer);
+
+        // Емкость жесткого диска
+        String hddCapacity = "512GB";
+        System.out.println("Емкость жесткого диска = " + hddCapacity);
+
+        // Определение HTTP
+        String httpDefinition = "Hypertext Transfer Protocol";
+        System.out.println("Определение HTTP = " + httpDefinition);
+
+        // Сокращенный URL
+        String shortUrl = "bit.ly/abc123";
+        System.out.println("Сокращенный URL = " + shortUrl);
+
+        // Новый идентификатор клиента
+        String newClientId = "CLNT-0001-XYZ";
+        System.out.println("Новый ID клиента = " + newClientId);
+
+        // Количество символов в ASCII
+        int asciiSymbolCount = 128;
+        System.out.println("Количество символов в ASCII = " + asciiSymbolCount);
+    }
+}
